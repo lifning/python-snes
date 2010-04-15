11ec621799b97f75ae01ccbe27b09e6645c265d5
Tim Allen <screwtape@froup.com>
2010-04-15 23:21:53 +1000

    Added the rest of the v064-final changes.

--- snes/_snes_wrapper.py
+++ snes/_snes_wrapper.py
@@ -7,8 +7,8 @@ import ctypes, atexit
 data_p = ctypes.POINTER(ctypes.c_ubyte)
 pixel_p = ctypes.POINTER(ctypes.c_uint16)
 
-video_refresh_cb_t = ctypes.CFUNCTYPE(None, ctypes.POINTER(ctypes.c_uint16),
-		ctypes.c_uint, ctypes.c_uint)
+video_refresh_cb_t = ctypes.CFUNCTYPE(None, pixel_p, ctypes.c_uint,
+		ctypes.c_uint)
 
 audio_sample_cb_t = ctypes.CFUNCTYPE(None, ctypes.c_uint16, ctypes.c_uint16)
 
@@ -35,9 +35,12 @@ load_cartridge_normal = None
 # When we're given a library path, set everything up.
 def init(libsnes_path):
 	global init, set_video_refresh, set_audio_sample
-	global set_input_poll, set_input_state, unload, run
-	global set_controller_port_device, serialize_size, serialize
+	global set_input_poll, set_input_state, set_controller_port_device
+	global power, reset, run, unload, serialize_size, serialize
 	global cheat_reset, cheat_set, load_cartridge_normal
+	global load_cartridge_bsx_slotted, load_cartridge_bsx
+	global load_cartridge_sufami_turbo, load_cartridge_super_game_boy
+	global get_memory_data, get_memory_size
 
 	libsnes = ctypes.cdll.LoadLibrary(libsnes_path)
 
@@ -67,17 +70,25 @@ def init(libsnes_path):
 	set_input_state.restype = None
 	set_input_state.argtypes = [input_state_cb_t]
 
-	unload = libsnes.snes_unload
-	unload.restype = None
-	unload.argtypes = []
+	set_controller_port_device = libsnes.snes_set_controller_port_device
+	set_controller_port_device.restype = None
+	set_controller_port_device.argtypes = [ctypes.c_bool, ctypes.c_uint]
+
+	power = libsnes.snes_power
+	power.restype = None
+	power.argtypes = []
+
+	reset = libsnes.snes_reset
+	reset.restype = None
+	reset.argtypes = []
 
 	run = libsnes.snes_run
 	run.restype = None
 	run.argtypes = []
 
-	set_controller_port_device = libsnes.snes_set_controller_port_device
-	set_controller_port_device.restype = None
-	set_controller_port_device.argtypes = [ctypes.c_bool, ctypes.c_uint]
+	unload = libsnes.snes_unload
+	unload.restype = None
+	unload.argtypes = []
 
 	serialize_size = libsnes.snes_serialize_size
 	serialize_size.restype = ctypes.c_uint
@@ -103,9 +114,42 @@ def init(libsnes_path):
 	load_cartridge_normal.restype = None
 	load_cartridge_normal.argtypes = [ctypes.c_char_p, data_p, ctypes.c_uint]
 
-	# TODO: Other cartridge types
+	load_cartridge_bsx_slotted = libsnes.snes_load_cartridge_bsx_slotted
+	load_cartridge_bsx_slotted.restype = None
+	load_cartridge_bsx_slotted.argtypes = [
+			ctypes.c_char_p, data_p, ctypes.c_uint,
+			ctypes.c_char_p, data_p, ctypes.c_uint,
+		]
 
-	# TODO: retrieving SRAM data
+	load_cartridge_bsx = libsnes.snes_load_cartridge_bsx
+	load_cartridge_bsx.restype = None
+	load_cartridge_bsx.argtypes = [
+			ctypes.c_char_p, data_p, ctypes.c_uint,
+			ctypes.c_char_p, data_p, ctypes.c_uint,
+		]
+
+	load_cartridge_sufami_turbo = libsnes.snes_load_cartridge_sufami_turbo
+	load_cartridge_sufami_turbo.restype = None
+	load_cartridge_sufami_turbo.argtypes = [
+			ctypes.c_char_p, data_p, ctypes.c_uint,
+			ctypes.c_char_p, data_p, ctypes.c_uint,
+			ctypes.c_char_p, data_p, ctypes.c_uint,
+		]
+
+	load_cartridge_super_game_boy = libsnes.snes_load_cartridge_super_game_boy
+	load_cartridge_super_game_boy.restype = None
+	load_cartridge_super_game_boy.argtypes = [
+			ctypes.c_char_p, data_p, ctypes.c_uint,
+			ctypes.c_char_p, data_p, ctypes.c_uint,
+		]
+
+	get_memory_data = libsnes.snes_get_memory_data
+	get_memory_data.restype = data_p
+	get_memory_data.argtypes = [ctypes.c_uint]
+
+	get_memory_size = libsnes.snes_get_memory_size
+	get_memory_size.restype = ctypes.c_uint
+	get_memory_size.argtypes = [ctypes.c_uint]
 
 	# The init method must only be called once, so we'll call it here but not
 	# export it from this module.
